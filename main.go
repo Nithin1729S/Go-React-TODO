cc16846eb45443676b97dd686879db6d2d263696
Nithin1729S <sureshnithin1729@gmail.com>
2024-10-13 01:04:05 +0530

    routes

--- main.go
+++ main.go
@@ -5,8 +5,35 @@ import (
 	"log"
 )
 
+
+type Todo struct {
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
+
+
 func main(){
 	fmt.Println("Hello World")
 	app:=fiber.New()
-	log.Fatal(app.Listen(":4000"))
+	todos:=[]Todo{}
+
+	app.Get("/",func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg":"hello world"})
+	} )
+
+	app.Post("/api/todos",func(c*fiber.Ctx)error{
+		todo:=&Todo{}
+		if err:=c.BodyParser(todo); err != nil{
+			return err
+		}
+		if todo.Body == "" {  //body empty
+			return c.Status(400).JSON(fiber.Map{"error":"Todo body cant be empty"})
+		}
+		todo.ID=len(todos)+1
+		todos=append(todos, *todo)
+		return c.Status(201).JSON(todo)
+
+	})
+	log.Fatal(app.Listen(":4001"))
 }
