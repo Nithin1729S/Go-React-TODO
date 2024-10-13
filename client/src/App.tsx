b9e3ef1c00502af0fe63d5c46c2c2bd1340484e7
Nithin1729S <sureshnithin1729@gmail.com>
2024-10-14 01:02:31 +0530

    new mutation hook add

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar'
 import TodoForm from './Components/TodoForm'
 import TodoList from './Components/TodoList'
 
+export const BASE_URL = "http://127.0.0.1:5000/api"
 function App() {
   return (
     <Stack h="100vh">
